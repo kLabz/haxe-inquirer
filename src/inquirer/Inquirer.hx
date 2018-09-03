ee6e80cb905afbbbf88c7742fca697f515885295
k <k@klabz.org>
2018-09-03 15:56:54 +0200

    Allow custom typing of Inquirer.prompt() return type

--- src/inquirer/Inquirer.hx
+++ src/inquirer/Inquirer.hx
@@ -6,11 +6,10 @@ import js.Promise;
 
 typedef Question = Dynamic; // TODO
 typedef Answer = EitherType<Bool, String>;
-typedef Answers = DynamicAccess<Answer>;
 
 @:jsRequire('inquirer')
 extern class Inquirer {
-	public static function prompt(questions:Array<Question>):Promise<Answers>;
+	public static function prompt<TAnswers>(questions:Array<Question>):Promise<TAnswers>;
 	public static function registerPrompt(name:String, prompt:Dynamic):Void; // TODO
 	public static function createPromptModule<T>():Array<Question>->Promise<T>;
 }
